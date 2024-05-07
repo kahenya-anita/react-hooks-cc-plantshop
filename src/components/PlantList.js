6bc519abf74bbb1f8c8cec52a41805c50e451c1f
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2024-05-07 10:22:46 +0300

    Fetch plants

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
+//1) Fetching data using Fetch
+//2) React hooks
 
 function PlantList() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((plants) => setPlants(plants));
+  }, []);
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plants) => (
+        <PlantCard
+          key={plants.id}
+          name={plants.name}
+          image={plants.image}
+          price={plants.price}
+        />
+      ))}
+    </ul>
   );
 }
 
